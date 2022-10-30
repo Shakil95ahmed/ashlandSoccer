4fda44545ad05dc608cace44637e16c9e426ba34
Shakil Ahmed <shakil.ah95@yahoo.com>
2022-10-30 19:40:02 -0400

    Add project files.

---
+++ Default.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Ashland Soccer</title>
+    <style type="text/css">
+        .auto-style1 {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="auto-style1">
+            <div class="auto-style1">
+            <img  src="AshlandSoccer.jpg" alt="ashland soccer" style="height: 200px; width: 500px" /><br />
+            <asp:Label ID="lblErrorMessage" runat="server" BorderStyle="None"></asp:Label>
+            <br />
+            <asp:Label ID="Label3" runat="server" height="19px" Text="Field" width="38px"></asp:Label>
+            <asp:DropDownList ID="ddField" runat="server" AutoPostBack="True">
+            </asp:DropDownList>
+            <asp:Label ID="Label2" runat="server" Text="Team" height="19px" width="38px"></asp:Label>
+            <asp:DropDownList ID="ddTeam" runat="server" AutoPostBack="True">
+            </asp:DropDownList>
+            <asp:Label ID="Label1" runat="server" height="19px" Text="Status" width="40px"></asp:Label>
+            <asp:DropDownList ID="ddStatus" runat="server" AutoPostBack="True">
+            </asp:DropDownList>
+            <br />
+            <br />
+            <%--<asp:Button ID="btnSubmit" runat="server" OnClick="btnSubmit_Click" Text="Submit" Width="330px" />--%>
+            <br />
+            <asp:ListBox ID="lbField" runat="server" Visible="False"></asp:ListBox>
+            <asp:ListBox ID="lbTeam" runat="server" Visible="False"></asp:ListBox>
+            <asp:ListBox ID="lbStatus" runat="server" Visible="False"></asp:ListBox>
+            <br />
+            </div>
+            <asp:GridView ID="gvDisplay" runat="server" style="text-align: center" HorizontalAlign="Center">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Web;
+using System.Web.UI.WebControls;
+
+
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        lblErrorMessage.Text = "";
+        string sqlStatement;
+        //sqlStatement = "SELECT * from schedule";
+        //sqlStatement = "SELECT Week, Time_Slot, Field, Home, Visitor, Status from SCHEDULE ORDER BY SCHEDULE.WEEK";
+
+        //sqlStatement = "SELECT DATES.PLAY_DATE, Time_Slot, Field, Home, Visitor, Status from SCHEDULE, DATES WHERE DATES.WEEK = SCHEDULE.WEEK ORDER BY SCHEDULE.WEEK";
+        //sqlStatement = "SELECT DATES.PLAY_DATE, GAMETIME.GAME_TIME, FIELDS.FIELD_NAME,TEAMNAME.TEAM_NAME, TEAMS_1.TEAM_NAME, STATUS.FULL_STATUS from SCHEDULE, DATES, GAMETIME, FIELDS,STATUS, TEAMNAME, TEAMNAME AS TEAMS_1 WHERE DATES.WEEK = SCHEDULE.WEEK AND GAMETIME.TIME_SLOT =SCHEDULE.TIME_SLOT AND FIELDS.FIELD = SCHEDULE.FIELD AND STATUS.STATUS = SCHEDULE.STATUS AND TEAMNAME.TEAM_NO = SCHEDULE.HOME AND TEAMS_1.TEAM_NO = SCHEDULE.VISITOR ORDER BY SCHEDULE.WEEK";
+        if (!IsPostBack)
+        {
+            myDatabaseConnection.fillDropDownList(ddTeam, lbTeam, "TEAMNAME", "TEAM_NAME", "TEAM_NO", ref
+           lblErrorMessage);
+            myDatabaseConnection.fillDropDownList(ddField, lbField, "FIELDS", "FIELD_NAME", "FIELD", ref
+           lblErrorMessage);
+            myDatabaseConnection.fillDropDownList(ddStatus, lbStatus, "STATUS", "FULL_STATUS", "STATUS", ref
+           lblErrorMessage);
+        }
+
+
+        sqlStatement = "SELECT DATES.PLAY_DATE, GAMETIME.GAME_TIME, FIELDS.FIELD_NAME,";
+        sqlStatement += " TEAMNAME.TEAM_NAME, TEAMS_1.TEAM_NAME, STATUS.FULL_STATUS";
+        sqlStatement += " from SCHEDULE, DATES, GAMETIME, FIELDS, STATUS, TEAMNAME, TEAMNAME AS TEAMS_1";
+        sqlStatement += " WHERE DATES.WEEK = SCHEDULE.WEEK";
+        sqlStatement += " AND GAMETIME.TIME_SLOT = SCHEDULE.TIME_SLOT";
+        sqlStatement += " AND FIELDS.FIELD = SCHEDULE.FIELD";
+        sqlStatement += " AND STATUS.STATUS = SCHEDULE.STATUS";
+        sqlStatement += " AND TEAMNAME.TEAM_NO = SCHEDULE.HOME";
+        sqlStatement += " AND TEAMS_1.TEAM_NO = SCHEDULE.VISITOR";
+        //if ddStatus selected
+        if (ddStatus.SelectedIndex > 0)
+        {
+            sqlStatement += " AND SCHEDULE.STATUS = '" + lbStatus.Items[ddStatus.SelectedIndex - 1] + "'";
+        }
+        //if ddField selected
+        if (ddField.SelectedIndex > 0)
+        {
+            sqlStatement += " AND SCHEDULE.FIELD = " + lbField.Items[ddField.SelectedIndex - 1];
+        }
+        //if ddTeam selected
+        if (ddTeam.SelectedIndex > 0)
+        {
+            sqlStatement += " AND (SCHEDULE.HOME = " + lbTeam.Items[ddTeam.SelectedIndex - 1] + "OR SCHEDULE.VISITOR = " + lbTeam.Items[ddTeam.SelectedIndex - 1] + ")";
+        }
+
+        sqlStatement += " ORDER BY SCHEDULE.WEEK";
+        myDatabaseConnection.executeSQL(sqlStatement, ref gvDisplay, ref lblErrorMessage);
+
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        lblErrorMessage.Text = "";
+        string sqlStatement;
+        
+
+        if (!IsPostBack)
+        {
+            myDatabaseConnection.fillDropDownList(ddTeam, lbTeam, "TEAMNAME", "TEAM_NAME", "TEAM_NO", ref
+            lblErrorMessage);
+            myDatabaseConnection.fillDropDownList(ddField, lbField, "FIELDS", "FIELD_NAME", "FIELD", ref
+            lblErrorMessage);
+            myDatabaseConnection.fillDropDownList(ddStatus, lbStatus, "STATUS", "FULL_STATUS", "STATUS",
+            ref lblErrorMessage);
+        }
+
+        sqlStatement = "SELECT DATES.PLAY_DATE, GAMETIME.GAME_TIME, FIELDS.FIELD_NAME,";
+        sqlStatement += " TEAMNAME.TEAM_NAME, TEAMS_1.TEAM_NAME, STATUS.FULL_STATUS";
+        sqlStatement += " from SCHEDULE, DATES, GAMETIME, FIELDS, STATUS, TEAMNAME, TEAMNAME AS TEAMS_1";
+        sqlStatement += " WHERE DATES.WEEK = SCHEDULE.WEEK";
+        sqlStatement += " AND GAMETIME.TIME_SLOT = SCHEDULE.TIME_SLOT";
+        sqlStatement += " AND FIELDS.FIELD = SCHEDULE.FIELD";
+        sqlStatement += " AND STATUS.STATUS = SCHEDULE.STATUS";
+        sqlStatement += " AND TEAMNAME.TEAM_NO = SCHEDULE.HOME";
+        sqlStatement += " AND TEAMS_1.TEAM_NO = SCHEDULE.VISITOR";
+
+        //if ddStatus selected
+        if (ddStatus.SelectedIndex > 0)
+        {
+            sqlStatement += " AND SCHEDULE.STATUS = '" + lbStatus.Items[ddStatus.SelectedIndex - 1] + "'";
+        }
+        //if ddField selected
+        if (ddField.SelectedIndex > 0)
+        {
+            sqlStatement += " AND SCHEDULE.FIELD = " + lbField.Items[ddField.SelectedIndex - 1];
+        }
+        //if ddTeam selected
+        if (ddTeam.SelectedIndex > 0)
+        {
+            sqlStatement += " AND (SCHEDULE.HOME = " + lbTeam.Items[ddTeam.SelectedIndex - 1] + "OR SCHEDULE.VISITOR = " + lbTeam.Items[ddTeam.SelectedIndex - 1] + ")";
+        }
+        
+        sqlStatement += " ORDER BY SCHEDULE.WEEK";
+        myDatabaseConnection.executeSQL(sqlStatement, ref gvDisplay, ref lblErrorMessage);
+    }
+}
